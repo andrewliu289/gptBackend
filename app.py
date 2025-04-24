4bb9a685265a980730af0d7d2abb8bfc401aa09c
Andrew Liu <andrew.liu289@gmail.com>
2025-04-24 00:28:58 -0700

    flask cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from gpt import GPTModelHandler
 import torch
 
 app = Flask(__name__)
+CORS(app)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 gpt_model_handler = GPTModelHandler(device)
