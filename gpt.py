0bf16f1471c9a90d69070c3fed8baaf51406ac74
Andrew Liu <andrew.liu289@gmail.com>
2025-04-27 16:19:02 -0700

    more lines

--- gpt.py
+++ gpt.py
@@ -61,8 +61,11 @@ class GPTModelHandler:
                     max_new_tokens=128,
                     temperature=0.7,
                     top_p=0.9,
+                    repetition_penalty=1.2,
                     do_sample=True,
-                    early_stopping=True
+                    early_stopping=True,
+                    eos_token_id=self.tokenizer.eos_token_id,  # Stop at eos
+                    pad_token_id=self.tokenizer.pad_token_id
                 )
 
             generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
